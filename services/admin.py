e409a505bc053eb18121712f11a9c41e1cdca94e
zasia <zasia.zafreen@gmail.com>
2024-03-28 13:39:57 +0600

    Models Created

---
+++ services/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from services.models import *
+
+
+# Register your models here.
+admin.site.register(Employee)
+
+admin.site.register(Device)
+
+admin.site.register(Allocation)
---
+++ services/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(unique=True, max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    
+class Device(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=300)
+    type = models.CharField(max_length=200)
+    condition = models.CharField(max_length=5000)
+    specification = models.CharField(max_length=5000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.name
+
+
+class Allocation(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    device_id = models.ForeignKey(Device, on_delete=models.CASCADE)
+    employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    assigned_condition = models.CharField(max_length=5000)
+    returned_condition = models.CharField(max_length=5000)
+    assigned_at = models.DateTimeField(auto_now_add=True)
+    returned_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.name    
+    
